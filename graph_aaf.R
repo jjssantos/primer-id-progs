1c46a50a2c9753764cf1d0d0171d2961db6fd330
Andrew Oler <andrew.oler@nih.gov>
2016-01-15 00:37:11 -0500

    Modified graph_aaf.R to work with output of add_consensus_columns_to_frequency_tables.pl without first running through merge_tally_overlapping_regions.pl.  Adds 'position' column and makes coloring based on 'merged' column optional, only if that column exists.

--- graph_aaf.R
+++ graph_aaf.R
@@ -8,11 +8,13 @@
 
 # graph_aaf.R <input>
 # Makes a scatterplot of alternate allele frequency by position
+# Takes output of add_consensus_columns_to_frequency_tables.pl or ( add_consensus_columns_to_frequency_tables.pl + merge_tally_overlapping_regions.pl ) 
 args <- commandArgs(TRUE)
 pdfname <- sub("[.][^.]*$", ".pdf", args[1], perl=TRUE)
 #print(pdfname)
 data <- read.delim(args[1])
-data$freqNonConsensus = data$numNonConsensus/data$coverageDepth
+#data$freqNonConsensus = data$numNonConsensus/data$coverageDepth
+data$freqNonConsensus = data$numNonConsensus/data$unambigCoverageDepth
 meanfreq <- mean(data$freqNonConsensus)
 medianfreq <- median(data$freqNonConsensus)
 medianfreq.round <- signif(medianfreq, digits=3)
@@ -20,6 +22,24 @@ print(paste("Replacing zero with median frequency:", medianfreq.round))
 # How do I get the average background??  medianfreq is closer to the average background.
 for (i in row.names(data)){if(data[i,"freqNonConsensus"] == 0){ data[i,"freqNonConsensus"] = medianfreq } }		# Replace zero values with median frequency
 
+# Make sure we have a 'position' column.
+if (!('position' %in% colnames(data))){
+	#print("no position");
+	if ('aminoAcidPosition' %in% colnames(data)){
+		data$position = data$aminoAcidPosition
+	} else if ('nucleotidePosition' %in% colnames(data)){
+		data$position = data$nucleotidePosition;
+	} else {
+		print("no position column")
+		quit(save = "no", status = 1, runLast = FALSE)
+	}
+}
+
+# Make sure we have a 'merged' column.
+#if (!('merged' %in% colnames(data))){
+#	# If no 'merged' column, set all values to 'F'
+#	data$merged = "F"	
+#}
 
 #print(head(data))
 library(ggplot2)
@@ -27,6 +47,11 @@ library(ggplot2)
 pdf(pdfname, width=12, height=8)
 #p <- ggplot(data, aes(x=position, y=freqNonConsensus)) + geom_point(stat = "identity") + theme_bw() + coord_trans(y="log") + scale_y_continuous(limits=c(0.00001,1), breaks=c(0.00001,0.0001,0.001,0.01,0.1,1), labels=c("0.00001","0.0001","0.001","0.01","0.1","1"), minor_breaks = c(0.00005,0.0005,0.005,0.05,0.5)) + labs(x="Position", y="Alternate Allele Frequency") + theme(axis.text.x = element_text(size=9, hjust=1, vjust = 0.5, angle=90 )) 
 #p + geom_hline(yintercept=medianfreq) + annotate("text", x=max(data$position) * 1.05, y=medianfreq * 1.2, label=paste("Median:", medianfreq.round), size=2) 
-p <- ggplot(data, aes(x=position, y=freqNonConsensus, color=merged)) + geom_point(stat = "identity") + theme_bw() + coord_trans(y="log") + scale_y_continuous(limits=c(0.00001,1), breaks=c(0.00001,0.0001,0.001,0.01,0.1,1), labels=c("0.00001","0.0001","0.001","0.01","0.1","1"), minor_breaks = c(0.00005,0.0005,0.005,0.05,0.5)) + labs(x="Position", y="Alternate Allele Frequency") + theme(axis.text.x = element_text(size=9, hjust=1, vjust = 0.5, angle=90 )) + scale_colour_manual(values = c("TRUE" = "red","FALSE" = "black"))
+if ('merged' %in% colnames(data)){
+	p <- ggplot(data, aes(x=position, y=freqNonConsensus, color=merged))
+} else {
+	p <- ggplot(data, aes(x=position, y=freqNonConsensus))
+} 
+p <- p + geom_point(stat = "identity") + theme_bw() + coord_trans(y="log") + scale_y_continuous(limits=c(0.00001,1), breaks=c(0.00001,0.0001,0.001,0.01,0.1,1), labels=c("0.00001","0.0001","0.001","0.01","0.1","1"), minor_breaks = c(0.00005,0.0005,0.005,0.05,0.5)) + labs(x="Position", y="Alternate Allele Frequency") + theme(axis.text.x = element_text(size=9, hjust=1, vjust = 0.5, angle=90 )) + scale_colour_manual(values = c("TRUE" = "red","FALSE" = "black"))
 p + geom_hline(yintercept=medianfreq) + annotate("text", x=max(data$position) * 1.05, y=medianfreq * 1.2, label=paste("Median:", medianfreq.round), size=2) 
 dev.off()
