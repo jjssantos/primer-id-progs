c56b3a4035c63ce180fda16cd263f170d7076c13
Andrew Oler <andrew.oler@nih.gov>
2017-12-05 18:10:44 -0500

    Adding option to specify minimum acceptable threshold primerID group size to compute_cutoff.  Closes #6.

--- compute_cutoff.pl
+++ compute_cutoff.pl
@@ -3,20 +3,27 @@
 $| = 1;
 use warnings;
 use aomisc;
+use Getopt::Long;
 
+my $min = 5; # Default value of 5
+GetOptions(
+	'min=s' => \$min, 
+);
 
 my $usage = "
-compute_cutoff.pl <max primerID group size in dataset>
+compute_cutoff.pl [options] <max primerID group size in dataset>
 This script computes the minimum reliable primerID group size.
 The formula is based on a simulation using the consensus_cutoff.rb 
 script from Zhou et al, J Virol, 2015 for a 12bp primerID.
+OPTIONS:
+--min 	Minimum acceptable threshold number of reads in a primerID group.  
+		Even if the computed cutoff is below this, the script won't report
+		a value below this minimum. Default is 5.  Suggested 3 to 5.
 ";
 
 die $usage unless $ARGV[0];
 my $m = $ARGV[0];
 my $cutoff = -6E-22 * $m ** 6 + 0.00000000000000002 * $m ** 5 - 0.0000000000003 * $m ** 4 + 0.000000002 * $m ** 3 - 0.000006 * $m ** 2 + 0.0183 * $m + 2.2794;
-
-my $min = 5;		# Modify this to your personal minimum acceptable threshold.  Suggested 3 to 5.  Even if $cutoff is below this, the script won't report a value below this minimum.
 
 $cutoff = $min if ($cutoff < $min);
 $cutoff = round($cutoff);
